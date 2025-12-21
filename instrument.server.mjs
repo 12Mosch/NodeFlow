88ec8e857628a602c830015505b1d99076b872ef
12Mosch <mosch12@tutanota.de>
2025-12-21 17:16:46 +0100

    fix: Production environment will incorrectly default to 'development'

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/tanstackstart-react'
 Sentry.init({
   dsn: process.env.VITE_SENTRY_DSN || process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV || 'development',
+  environment: process.env.NODE_ENV || 'production',
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/tanstackstart-react/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
