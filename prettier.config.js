2714a9d1907396e4e7661acefd85329022cfc85e
12Mosch <mosch12@tutanota.de>
2025-12-29 13:21:28 +0100

    feat: Update ESLint configuration and dependencies
    - Added new ESLint plugins: , , , and  for enhanced linting capabilities.
    - Updated ESLint configuration to include the new plugins and integrated Prettier for consistent code formatting.
    - Updated package dependencies in  and  to include the latest versions of the added plugins and related packages.
    - Configured Prettier to use the  for improved Tailwind CSS support.
    
    This update enhances code quality and ensures better adherence to coding standards across the project.

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,8 @@ const config = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles.css',
 }
 
 export default config
